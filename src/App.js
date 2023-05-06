20068ce5046194747dc27dfa3b4c414350ad7e1d
nadarajan.s <nadarajan.s@gofrugal.com>
2023-05-06 12:55:01 +0530

    [SN] Overlay added for api load handling

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
+import { useContext } from 'react';
 import './App.css';
 import Container from './Components/Container';
+import CircularProgress from '@mui/material/CircularProgress';
+import { KenContext } from './Context/KenContext';
 
 function App() {
+    const { isShow } = useContext(KenContext);
   return (
-    <div className="App">
+      <div className="App">
+          {isShow && <div className="overlay">
+              <div className="loader"><CircularProgress /></div>
+          </div>}
         <Container/>
     </div>
   );
